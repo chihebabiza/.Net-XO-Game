bb74cfa36c46fe776261344e5161d9a526839211
chiheb abiza <chihebabiza@gmail.com>
2025-06-30 10:03:29 +0100

    edit ui

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,10 +44,6 @@
             this.btnRestart = new System.Windows.Forms.Button();
             this.lblWinnerIs = new System.Windows.Forms.Label();
             this.lblWinner = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button7
@@ -58,9 +54,10 @@
             this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button7.Location = new System.Drawing.Point(422, 496);
+            this.button7.Location = new System.Drawing.Point(633, 763);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(150, 103);
+            this.button7.Size = new System.Drawing.Size(225, 158);
             this.button7.TabIndex = 8;
             this.button7.Tag = "?";
             this.button7.UseVisualStyleBackColor = false;
@@ -74,9 +71,10 @@
             this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button8.Location = new System.Drawing.Point(648, 496);
+            this.button8.Location = new System.Drawing.Point(972, 763);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(150, 103);
+            this.button8.Size = new System.Drawing.Size(225, 158);
             this.button8.TabIndex = 7;
             this.button8.Tag = "?";
             this.button8.UseVisualStyleBackColor = false;
@@ -90,9 +88,10 @@
             this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button9.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button9.Location = new System.Drawing.Point(876, 496);
+            this.button9.Location = new System.Drawing.Point(1314, 763);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(150, 103);
+            this.button9.Size = new System.Drawing.Size(225, 158);
             this.button9.TabIndex = 6;
             this.button9.Tag = "?";
             this.button9.UseVisualStyleBackColor = false;
@@ -106,9 +105,10 @@
             this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button4.Location = new System.Drawing.Point(422, 323);
+            this.button4.Location = new System.Drawing.Point(633, 497);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 103);
+            this.button4.Size = new System.Drawing.Size(225, 158);
             this.button4.TabIndex = 5;
             this.button4.Tag = "?";
             this.button4.UseVisualStyleBackColor = false;
@@ -122,9 +122,10 @@
             this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button5.Location = new System.Drawing.Point(648, 326);
+            this.button5.Location = new System.Drawing.Point(972, 502);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 103);
+            this.button5.Size = new System.Drawing.Size(225, 158);
             this.button5.TabIndex = 4;
             this.button5.Tag = "?";
             this.button5.UseVisualStyleBackColor = false;
@@ -138,9 +139,10 @@
             this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button6.Location = new System.Drawing.Point(876, 330);
+            this.button6.Location = new System.Drawing.Point(1314, 508);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(150, 103);
+            this.button6.Size = new System.Drawing.Size(225, 158);
             this.button6.TabIndex = 3;
             this.button6.Tag = "?";
             this.button6.UseVisualStyleBackColor = false;
@@ -154,9 +156,10 @@
             this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button3.Location = new System.Drawing.Point(876, 163);
+            this.button3.Location = new System.Drawing.Point(1314, 251);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 103);
+            this.button3.Size = new System.Drawing.Size(225, 158);
             this.button3.TabIndex = 2;
             this.button3.Tag = "?";
             this.button3.UseVisualStyleBackColor = false;
@@ -170,9 +173,10 @@
             this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button2.Location = new System.Drawing.Point(648, 160);
+            this.button2.Location = new System.Drawing.Point(972, 246);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 103);
+            this.button2.Size = new System.Drawing.Size(225, 158);
             this.button2.TabIndex = 1;
             this.button2.Tag = "?";
             this.button2.UseVisualStyleBackColor = false;
@@ -186,9 +190,10 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::XO_Game_Final.Properties.Resources.question_mark_96;
-            this.button1.Location = new System.Drawing.Point(422, 160);
+            this.button1.Location = new System.Drawing.Point(633, 246);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 103);
+            this.button1.Size = new System.Drawing.Size(225, 158);
             this.button1.TabIndex = 0;
             this.button1.Tag = "?";
             this.button1.UseVisualStyleBackColor = false;
@@ -200,12 +205,12 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Britannic Bold", 50F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(449, 26);
+            this.label1.Location = new System.Drawing.Point(674, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(545, 74);
+            this.label1.Size = new System.Drawing.Size(814, 111);
             this.label1.TabIndex = 9;
             this.label1.Text = "Tic-Tac-Toe Game";
-         
             // 
             // label2
             // 
@@ -213,22 +218,23 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Britannic Bold", 36F);
             this.label2.ForeColor = System.Drawing.Color.Yellow;
-            this.label2.Location = new System.Drawing.Point(139, 181);
+            this.label2.Location = new System.Drawing.Point(208, 278);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 53);
+            this.label2.Size = new System.Drawing.Size(184, 80);
             this.label2.TabIndex = 10;
             this.label2.Text = "Turn";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-          
             // 
             // lblTurn
             // 
             this.lblTurn.BackColor = System.Drawing.Color.Transparent;
             this.lblTurn.Font = new System.Drawing.Font("Britannic Bold", 36F);
             this.lblTurn.ForeColor = System.Drawing.Color.White;
-            this.lblTurn.Location = new System.Drawing.Point(27, 235);
+            this.lblTurn.Location = new System.Drawing.Point(40, 362);
+            this.lblTurn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTurn.Name = "lblTurn";
-            this.lblTurn.Size = new System.Drawing.Size(377, 51);
+            this.lblTurn.Size = new System.Drawing.Size(566, 78);
             this.lblTurn.TabIndex = 11;
             this.lblTurn.Text = "Player 1";
             this.lblTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -239,9 +245,10 @@
             this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRestart.Font = new System.Drawing.Font("Britannic Bold", 27.75F);
             this.btnRestart.ForeColor = System.Drawing.Color.White;
-            this.btnRestart.Location = new System.Drawing.Point(89, 446);
+            this.btnRestart.Location = new System.Drawing.Point(134, 686);
+            this.btnRestart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRestart.Name = "btnRestart";
-            this.btnRestart.Size = new System.Drawing.Size(260, 56);
+            this.btnRestart.Size = new System.Drawing.Size(390, 86);
             this.btnRestart.TabIndex = 12;
             this.btnRestart.Text = "Restart Game";
             this.btnRestart.UseVisualStyleBackColor = true;
@@ -253,9 +260,10 @@
             this.lblWinnerIs.BackColor = System.Drawing.Color.Transparent;
             this.lblWinnerIs.Font = new System.Drawing.Font("Britannic Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWinnerIs.ForeColor = System.Drawing.Color.Yellow;
-            this.lblWinnerIs.Location = new System.Drawing.Point(117, 299);
+            this.lblWinnerIs.Location = new System.Drawing.Point(176, 460);
+            this.lblWinnerIs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWinnerIs.Name = "lblWinnerIs";
-            this.lblWinnerIs.Size = new System.Drawing.Size(172, 53);
+            this.lblWinnerIs.Size = new System.Drawing.Size(259, 80);
             this.lblWinnerIs.TabIndex = 13;
             this.lblWinnerIs.Text = "Winner";
             this.lblWinnerIs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -265,42 +273,21 @@
             this.lblWinner.BackColor = System.Drawing.Color.Transparent;
             this.lblWinner.Font = new System.Drawing.Font("Britannic Bold", 36F);
             this.lblWinner.ForeColor = System.Drawing.Color.Lime;
-            this.lblWinner.Location = new System.Drawing.Point(76, 363);
+            this.lblWinner.Location = new System.Drawing.Point(114, 558);
+            this.lblWinner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWinner.Name = "lblWinner";
-            this.lblWinner.Size = new System.Drawing.Size(273, 51);
+            this.lblWinner.Size = new System.Drawing.Size(410, 78);
             this.lblWinner.TabIndex = 14;
             this.lblWinner.Text = "In Progress";
             this.lblWinner.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(71, 531);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(322, 88);
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(121, 20);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(161, 142);
-            this.pictureBox2.TabIndex = 16;
-            this.pictureBox2.TabStop = false;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(1109, 668);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1664, 1028);
             this.Controls.Add(this.lblWinner);
             this.Controls.Add(this.lblWinnerIs);
             this.Controls.Add(this.btnRestart);
@@ -320,14 +307,13 @@
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.RightToLeftLayout = true;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic-Tac-Toe Game";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -350,8 +336,6 @@
         private System.Windows.Forms.Button btnRestart;
         private System.Windows.Forms.Label lblWinnerIs;
         private System.Windows.Forms.Label lblWinner;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
